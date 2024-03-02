c339a51ff852c4cb98b38bb4282f141fe2770a36
GabrielScarpelinDiniz <159063078+GabrielScarpelinDiniz@users.noreply.github.com>
2024-03-01 23:33:51 -0300

    Corrigido erro de responsividade

--- scenes/menu.js
+++ scenes/menu.js
@@ -8,6 +8,8 @@ class Menu extends Phaser.Scene {
   create() {
     
     this.add.image(0, 75, 'menu').setOrigin(0, 0).setScale(1200 / 1280); // Adiciona a imagem de fundo, seta a origem e escala
+    this.add.text(0, 650, "Utilize as setas para mover o personagem.", { fontSize: "32px", fill: "#000" }); // Texto
+    this.add.text(0, 700, "Dica: Encoste somente em cima dos inimigos", { fontSize: "24px", fill: "#000" }); // Texto
     this.input.on('pointerdown', function () { // Evento de clique na tela
       // Inicia o jogo
       this.scene.start('Game');
